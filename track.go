20aba9298c3240fc74c105642191c95a54fe7ba5
François Triquet <francois.triquet@recast.ai>
2017-06-26 22:55:10 +0200

    [UPD] track validation

--- track.go
+++ track.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"errors"
+	"net/url"
 
 	"github.com/jinzhu/gorm"
 )
 
+// MusicFolder linter break my balls
+const MusicFolder = "./music"
+
+// Track lol
 type Track struct {
 	gorm.Model
-	YoutubeURL string `gorm:"column:youtube_url"`
-	Title      string `gorm:"column:title"`
-	Upvotes    uint64 `gorm:"column:upvotes"`
-	Downvotes  uint64 `gorm:"column:downvotes"`
-	Filename   string `gorm:"column:filename"`
+	YoutubeURL string `gorm:"column:youtube_url" json:"youtube_url"`
+	Title      string `gorm:"column:title" json:"title"`
+	Upvotes    uint64 `gorm:"column:upvotes" json:"upvotes"`
+	Downvotes  uint64 `gorm:"column:downvotes" json:"downvotes"`
+	Filename   string `gorm:"column:filename" json:"filename"`
 }
 
+// BeforeSave stuff
 func (t *Track) BeforeSave() error {
 	if len(t.YoutubeURL) == 0 {
 		return errors.New("Empty Youtube Url")
@@ -25,7 +31,18 @@ func (t *Track) BeforeSave() error {
 	}
 
 	if len(t.Filename) == 0 {
-		return errors.New("Empty Title")
+		url, err := url.Parse(t.YoutubeURL)
+		if err != nil {
+			return err
+		}
+		viewIDs := url.Query().Get("v")
+		if len(viewIDs) == 0 {
+			return errors.New("Invalid youtube url")
+		}
+		viewID := viewIDs[0]
+		filename := MusicFolder + "/" + string(viewID) + ".mp3"
+
+		t.Filename = filename
 	}
 
 	return nil
