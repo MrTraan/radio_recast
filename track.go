1422a506b66e60e018cd48810bbe8a36c9c57330
François Triquet <francois.triquet@recast.ai>
2017-06-26 21:09:56 +0200

    add gorm hints to track struct

--- track.go
+++ track.go
@@ -4,9 +4,9 @@ import "github.com/jinzhu/gorm"
 
 type Track struct {
 	gorm.Model
-	YoutubeURL string
-	Title      string
-	Upvotes    uint64
-	Downvotes  uint64
-	Filename   string
+	YoutubeURL string `gorm:"column:youtube_url"`
+	Title      string `gorm:"column:title"`
+	Upvotes    uint64 `gorm:"column:upvotes"`
+	Downvotes  uint64 `gorm:"column:downvotes"`
+	Filename   string `gorm:"column:filname"`
 }
